cdd84201a0b9838c6585e150930b00b67e71ae74
wsharp <wsharp@ltu.edu>
2014-06-06 09:05:13 -0400

    initial push

---
+++ app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by wsharp on 6/6/2014.
+ */
+var server = require("./server");
+
+server.start();
---
+++ server.js
@@ -0,0 +1,66 @@
+/**
+ * Created by wsharp on 6/6/2014.
+ */
+var http = require("http");
+var path = require("path");
+var mime = require('mime');
+var fs = require("fs");
+
+function start() {
+
+    function writeHeader(response,pathName){
+        response.writeHead(200, {"Content-Type": mime.lookup(pathName) });
+    }
+
+    //helper function handles file verification
+    function getFile(filePath,res,page404){
+        console.log("File Path: " + filePath);
+        //does the requested file exist?
+        fs.exists(filePath,function(exists){
+            //if it does...
+            if(exists){
+                //read the file, run the anonymous function
+                fs.readFile(filePath,function(err,contents){
+                    if(!err){
+                        //if there was no error
+                        //send the contents with the default 200/ok header
+                        writeHeader(res,filePath);
+                        res.end(contents);
+                    } else {
+                        //for our own troubleshooting
+                        console.dir(err);
+                    };
+                });
+            } else {
+                //if the requested file was not found
+                //serve-up our custom 404 page
+                fs.readFile(page404,function(err,contents){
+                    //if there was no error
+                    if(!err){
+                        //send the contents with a 404/not found header
+                        res.writeHead(404, {'Content-Type': 'text/html'});
+                        res.end(contents);
+                    } else {
+                        //for our own troubleshooting
+                        console.dir(err);
+                    };
+                });
+            };
+        });
+    };
+
+
+    function onRequest(request, response) {
+        var
+            fileName = request.url,
+            localFolder = __dirname + '/public/',
+            page404 = localFolder + '404.html';
+        if(fileName == '/'){fileName = 'index.html'}
+        getFile((localFolder + fileName),response,page404);
+    }
+
+    http.createServer(onRequest).listen(3000);
+    console.log("Server has started on port 3000.");
+}
+
+exports.start = start;

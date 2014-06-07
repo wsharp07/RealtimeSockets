4a54114a488c3380eadae8257ff27ca7ca43dd66
wsharp <wsharp@ltu.edu>
2014-06-07 08:58:17 -0400

    adding moment for better dates

--- app/database.js
+++ app/database.js
@@ -5,17 +5,6 @@ var mongoose = require("mongoose");
 var socket = require("./socketio");
 var logModel = require("./models/LogModel");
 
-function LoadAllLogs(err, logs){
-    if(err != 'null'){
-        logs.forEach(function(log){
-            socket.SendMessage(log);
-        })
-    }
-    else{
-        console.log(err);
-    }
-}
-
 exports.Connect = function(){
     mongoose.connect('mongodb://localhost/logdb');
     var db = mongoose.connection;
